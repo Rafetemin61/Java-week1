fbfa478ee02ef8359d46ce7346c68dc82420d42a
Rafet Emin Alpaslan <34487796+Rafetemin61@users.noreply.github.com>
2022-09-21 19:55:38 +0300

    My Workspace

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,37 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//for
+		
+		for (int i=1; i<=10; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("For Dongusu Bitti");
+		
+		//while
+		
+		int i = 2;
+		while(i<10) {
+			System.out.println(i);
+			i+=2;
+			
+			
+		}System.out.println("While Dongusu Bitti");
+		
+		// do-while dongusu
+		int j = 100;
+		do {
+			System.out.println(j);
+		j+=2;
+		
+		
+		}while(j<10);
+		
+		System.out.println("Do-While Dongusu Bitti");
+
+		
+		
+	} 
+}
