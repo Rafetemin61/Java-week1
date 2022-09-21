bfe746cadf087e8e54e4d6883379f09d1271a4fe
Rafet Emin Alpaslan <34487796+Rafetemin61@users.noreply.github.com>
2022-09-21 19:58:42 +0300

    My Workspace

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj ="Ogrenci sayisi";
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
